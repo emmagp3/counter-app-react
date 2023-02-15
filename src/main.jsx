761a5c10a7fc642ebddbda35febb149a70987387
Emmanuel Guadalupe <ema_110@outlook.es>
2023-02-14 23:12:45 -0600

    Main modificado: Ahora se renderiza CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client';
 import HelloWorldApp from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
 
+import './styles.css'
+import CounterApp from './CounterApp';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorldApp />
-    <FirstApp />
+    <CounterApp value={0}/>
   </React.StrictMode>
 );
