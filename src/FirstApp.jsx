1bf54ed351806755bb86e156078472028e77cb88
Emmanuel Guadalupe <ema_110@outlook.es>
2023-02-14 23:11:10 -0600

    Componente FirstApp agregado

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,3 +1,41 @@
-export const FirstApp = () => {
-  return (<h2>First App.</h2>)
+import PropTypes, { string } from 'prop-types'
+// import { Fragment } from "react";
+
+/**
+ * Cualquier variable/constante/expresión que no esté involucrada directamente con el componente
+ * o algún hook, es conveniente que sea declarada afuera del componente para que React no tenga
+ * que reprocesarlo.
+ */
+const newMessage = {
+  title: 'Mensaje de saludo',
+  message: 'Hola Emmanuel',
+};
+
+export const FirstApp = ({ 
+  title, 
+  subtitle, 
+}) => {
+  return (
+    // <Fragment>
+    <> { /* Misma forma de agrupar */}
+      <h1>{ title }</h1>
+      <h2>{ subtitle }</h2>
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      <p>Soy un texto</p>
+      <p>Soy un texto</p>
+      <p>Soy un texto</p>
+      <p>Soy un texto</p>
+    </>
+    // </Fragment>
+  );
+};
+
+FirstApp.propTypes = {
+  subtitle: PropTypes.string,
+  title: PropTypes.string.isRequired,
+}
+
+FirstApp.defaultProps = {
+  subtitle: "No hay subtítulo",
+  title: 'No hay título',
 }
