2cada04b3ad5d6ef77c96f657ea1129eceaab0b4
Emmanuel Guadalupe <ema_110@outlook.es>
2023-02-15 19:54:28 -0600

    Test para 07-deses-arr

---
+++ tests/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,11 @@
+import { retornaArreglo } from "../../src/base-pruebas/07-deses-arr";
+
+describe('Pruebas en 07-deses-arr', () => {
+  test('Debe retornar un string y un número', () => {
+    const [letters, numbers] = retornaArreglo();
+    expect(letters).toBe('ABC');
+    expect(numbers).toBe(123);
+    expect(letters).toEqual(expect.any(String));
+    expect(numbers).toEqual(expect.any(Number));
+  });
+});
