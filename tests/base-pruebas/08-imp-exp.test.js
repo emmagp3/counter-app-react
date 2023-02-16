907dc45abc763b6a022fea45a97ba1b7b96ab90e
Emmanuel Guadalupe <ema_110@outlook.es>
2023-02-16 15:22:51 -0600

    Tests de tarea implementados

--- tests/base-pruebas/08-imp-exp.test.js
+++ tests/base-pruebas/08-imp-exp.test.js
@@ -1,4 +1,5 @@
 import { getHeroById, getHeroesByOwner } from '../../src/base-pruebas/08-imp-exp';
+import heroes from '../../src/data/heores';
 
 describe('Pruebas en 08-imp-exp', () => {
   test('getHeroById debe retornar un héroe', () => {
@@ -15,6 +16,31 @@ describe('Pruebas en 08-imp-exp', () => {
     const testId = 100;
     const hero = getHeroById(testId);
     expect(hero).toBeFalsy();
+  });
+
+  /**
+   * Test de tarea: Debe retornar una arreglos con todos los héroes de DC. 
+   * (Aplicar toEqual al arreglo filtrado)
+   * Además, debe evaluarse la longitud del arreglo (Debe ser 3).
+   */
+  test('Debe retornar una arreglos con todos los héroes de DC', () => {
+    const testOwner = 'DC';
+    const dcHeroes = getHeroesByOwner(testOwner);
+
+    expect(dcHeroes).toEqual(heroes.filter(hero => hero.owner === testOwner));
+    expect(dcHeroes.length).toBe(3);
+  });
+
+  /**
+   * Test de tarea: Debe retornar una arreglos con todos los héroes de Marvel. Solo debe evaluarse la
+   * longitud del arreglo (Debe ser 2),
+   */
+  test('Debe retornar una arreglos con todos los héroes de Marvel', () => {
+    const testOwner = 'Marvel';
+    const marvelHeroes = getHeroesByOwner(testOwner);
+
+    expect(marvelHeroes).toEqual(heroes.filter(hero => hero.owner === testOwner));
+    expect(marvelHeroes.length).toBe(2);
   });
 });
 
